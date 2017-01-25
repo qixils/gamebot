37ef0636ae85e6aa0f75a4298bbcd765a010566b
Noahkiq <noahkiqgaming@gmail.com>
2017-01-25 17:59:25 -0500

    Initial commit

---
+++ gamebot/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using Discord;
+using Discord.Commands;
+
+namespace gamebot
+{
+	class gamebot
+	{
+		static void Main(string[] args) => new gamebot().Start();
+
+		private static DiscordClient _client = new DiscordClient();
+
+		public void Start()
+		{
+			string token = File.ReadAllText("token.txt");
+			_client.ExecuteAndWait(async () => {
+				await _client.Connect(token, TokenType.Bot);
+			});
+
+		}
+	}
+}
