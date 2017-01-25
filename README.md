58c7d793586e5c3e32e07100b16f552d96762952
Noahkiq <noahkiqgaming@gmail.com>
2017-01-25 18:24:34 -0500

    Add placeholder code and readme

---
+++ README.md
@@ -0,0 +1,3 @@
+# gamebot
+
+A C# bot for [Discord](https://discordapp.com/) made to automate games.
--- gamebot/Program.cs
+++ gamebot/Program.cs
@@ -11,8 +11,25 @@ namespace gamebot
 
 		private static DiscordClient _client = new DiscordClient();
 
+		public static string prefix = "gb!";
+		public static int tttProgress = 0;
+
 		public void Start()
 		{
+			_client.Log.Message += (s, e) => Console.WriteLine($"[{e.Severity}] {e.Source}: {e.Message}");
+
+			_client.MessageReceived += async (s, e) =>
+			{
+				if (e.Message.Text == prefix + "tictactoe")
+				{
+					if (tttProgress = 0)
+					{
+						tttProgress++;
+						e.Channel.SendMessage("Initializing game...");
+					}
+				}
+			};
+
 			string token = File.ReadAllText("token.txt");
 			_client.ExecuteAndWait(async () => {
 				await _client.Connect(token, TokenType.Bot);
