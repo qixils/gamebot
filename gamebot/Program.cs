15b01622be9bc82d5ef91e6c69cf5aecb9d3d74d
Julien12150 <julienfillol2@gmail.com>
2017-01-26 01:10:00 +0100

    improved tic tac toe, still unfinished

--- gamebot/Program.cs
+++ gamebot/Program.cs
@@ -1,9 +1,8 @@
 using System;
-using System.Collections;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using Discord;
-using Discord.Commands;
 
 namespace gamebot
 {
@@ -14,7 +13,8 @@ namespace gamebot
 		private static DiscordClient _client = new DiscordClient();
 
 		public static string prefix = "gb!";
-		public static int tttProgress = 0;
+
+		List<TicTacToe> TTTGames = new List<TicTacToe>();
 
 		public void Start()
 		{
@@ -26,11 +26,49 @@ namespace gamebot
 				string[] par = e.Message.RawText.Split(' ').Skip(1).ToArray();
 				if (cmd == "tictactoe")
 				{
-					if (tttProgress == 0)
+					string helpNew = $"Type `{prefix}tictactoe new <mention>` to invite someone to play Tic Tac Toe.";
+					string helpPlay = $"Type `{prefix}tictactoe play <X> <Y>` to place a cross or a circle in a game.";
+					if (par.Length == 1)
 					{
-						tttProgress++;
-						await e.Channel.SendMessage("Initializing game...");
+						if (par[0] == "new")
+							await e.Channel.SendMessage(helpNew);
+						else if (par[0] == "play")
+							await e.Channel.SendMessage(helpPlay);
+						else
+							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
 					}
+					else if (par.Length == 2)
+					{
+						if (par[0] == "play")
+							await e.Channel.SendMessage(helpPlay);
+						else if (par[1] == "new")
+						{
+							User[] mentioned = e.Message.MentionedUsers.ToArray();
+							if (mentioned.Length != 1)
+								await e.Channel.SendMessage(helpNew);
+							else
+							{
+								TTTGames.Add(new TicTacToe(e.User, mentioned[0], e.Channel));
+							}
+						}
+						else
+							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
+
+					}
+					else if (par.Length == 3)
+					{
+						if (par[0] == "new")
+							await e.Channel.SendMessage(helpNew);
+						else if (par[1] == "play")
+						{
+							int i = TicTacToe.SearchPlayer(TTTGames.ToArray(), e.User);
+							TTTGames[i].TakeTurn(e.User, int.Parse(par[2]), int.Parse(par[3]));
+						}
+						else
+							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
+					}
+					else
+						await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
 				}
 			};
 
---
+++ gamebot/TicTacToe.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Discord;
+
+namespace gamebot
+{
+	public class TicTacToe
+	{
+		public User player1;
+		public User player2;
+		Channel channel;
+		bool?[,] game;
+
+		bool isPlayer2Turn;
+
+		public TicTacToe(User player1, User player2, Channel channel)
+		{
+			this.player1 = player1;
+			this.player2 = player2;
+			this.channel = channel;
+		}
+		public void TakeTurn(User player, int x, int y)
+		{
+			throw new NotImplementedException();
+		}
+		public string DrawGame()
+		{
+			throw new NotImplementedException();
+		}
+		public static int SearchPlayer(TicTacToe[] games, User player)
+		{
+			int r = -1;
+			for (int i = 0; i < games.Length; i++)
+			{
+				TicTacToe game = games[i];
+				if (game.player1 == player || game.player2 == player)
+				{
+					r = i;
+					break;
+				}
+			}
+			return r;
+		}
+	}
+}
