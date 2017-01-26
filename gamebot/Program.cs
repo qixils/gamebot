dddb055dfce6b4c512e35d6a667151bf2e42d00d
Noahkiq <noahkiqgaming@gmail.com>
2017-01-26 16:04:24 -0500

    Added some commenting

--- gamebot/Program.cs
+++ gamebot/Program.cs
@@ -12,7 +12,7 @@ namespace gamebot
 
 		private static DiscordClient _client = new DiscordClient();
 
-		public static string prefix = "gb!";
+		public static string prefix = "gb!"; // Sets custom bot prefix
 
 		List<TicTacToe> TTTGames = new List<TicTacToe>();
 
@@ -22,50 +22,50 @@ namespace gamebot
 
 			_client.MessageReceived += async (s, e) =>
 			{
-				string cmd = e.Message.RawText.Split(' ')[0].Skip(prefix.Length).ToString();
-				string[] par = e.Message.RawText.Split(' ').Skip(1).ToArray();
-				if (cmd == "tictactoe")
+				string cmd = e.Message.RawText.Split(' ')[0].Skip(prefix.Length).ToString(); // Grabs the command used by removing the prefix
+				string[] par = e.Message.RawText.Split(' ').Skip(1).ToArray(); // Grabs the arguments used in the command
+				if (cmd == "tictactoe") // tictactoe command code
 				{
 					string helpNew = $"Type `{prefix}tictactoe new <mention>` to invite someone to play Tic Tac Toe.";
 					string helpPlay = $"Type `{prefix}tictactoe play <X> <Y>` to place a cross or a circle in a game.";
-					if (par.Length == 1)
+					if (par.Length == 1) // checks if only one command argument was supplied
 					{
 						if (par[0] == "new")
-							await e.Channel.SendMessage(helpNew);
+							await e.Channel.SendMessage(helpNew); // outputs the 'helpNew' string if the argument was 'new'
 						else if (par[0] == "play")
-							await e.Channel.SendMessage(helpPlay);
+							await e.Channel.SendMessage(helpPlay); // same as above comment, but with 'helpPlay' string and 'play' arg
 						else
 							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
 					}
-					else if (par.Length == 2)
+					else if (par.Length == 2) // checks if two arguments were supplied
 					{
 						if (par[0] == "play")
-							await e.Channel.SendMessage(helpPlay);
-						else if (par[1] == "new")
+							await e.Channel.SendMessage(helpPlay); // too few requirements were supplied so help is shown
+						else if (par[0] == "new")
 						{
 							User[] mentioned = e.Message.MentionedUsers.ToArray();
 							if (mentioned.Length != 1)
-								await e.Channel.SendMessage(helpNew);
+								await e.Channel.SendMessage(helpNew); // too few (or many) users were mentioned, help is shown
 							else
 							{
-								TTTGames.Add(new TicTacToe(e.User, mentioned[0], e.Channel));
+								TTTGames.Add(new TicTacToe(e.User, mentioned[0], e.Channel)); // a new TTT game is added to 'TTTGames' with the command runner, opponent, and channel
 							}
 						}
 						else
-							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
+							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}"); // send default help message if no valid commands were detected
 
 					}
 					else if (par.Length == 3)
 					{
 						if (par[0] == "new")
-							await e.Channel.SendMessage(helpNew);
-						else if (par[1] == "play")
+							await e.Channel.SendMessage(helpNew); // too many requirements were supplied so help is shown
+						else if (par[0] == "play")
 						{
 							int i = TicTacToe.SearchPlayer(TTTGames.ToArray(), e.User);
 							TTTGames[i].TakeTurn(e.User, int.Parse(par[2]), int.Parse(par[3]));
 						}
 						else
-							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
+							await e.Channel.SendMessage($"{helpNew}\n{helpPlay}"); // invalid arguments given, help displayed
 					}
 					else
 						await e.Channel.SendMessage($"{helpNew}\n{helpPlay}");
--- gamebot/TicTacToe.cs
+++ gamebot/TicTacToe.cs
@@ -14,13 +14,14 @@ namespace gamebot
 
 		public TicTacToe(User player1, User player2, Channel channel)
 		{
+			// variables are updated to match game
 			this.player1 = player1;
 			this.player2 = player2;
 			this.channel = channel;
 		}
 		public void TakeTurn(User player, int x, int y)
 		{
-			throw new NotImplementedException();
+			throw new NotImplementedException(); // throw error as function isn't implemented yet
 		}
 		public string DrawGame()
 		{
@@ -52,16 +53,16 @@ namespace gamebot
 		public static int SearchPlayer(TicTacToe[] games, User player)
 		{
 			int r = -1;
-			for (int i = 0; i < games.Length; i++)
+			for (int i = 0; i < games.Length; i++) // iterate through all variables in 'games' by initially setting int 'i' to 0, checking if it's less than the total number of games, then adding one
 			{
 				TicTacToe game = games[i];
-				if (game.player1 == player || game.player2 == player)
+				if (game.player1 == player || game.player2 == player) // checks if command runner is player 1 or 2 in any active game
 				{
-					r = i;
-					break;
+					r = i; // sets 'r' as 'i'
+					break; // ends 'for' loop
 				}
 			}
-			return r;
+			return r; // returns 'r' (the game ID) back to initiating code
 		}
 	}
 }
