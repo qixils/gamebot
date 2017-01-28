db92a5c31e0410aa252c03506d5ccb9d17dbf673
Noahkiq <noahkiqgaming@gmail.com>
2017-01-28 11:09:27 -0500

    Add hangman WordChooser

---
+++ gamebot/Hangman.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Discord;
+using System.Collections.Generic;
+
+namespace gamebot
+{
+	public class Hangman
+	{
+		static Random rnd = new Random();
+
+		public string WordChooser()
+		{
+			var words = new List<string> { "stabyourself", "meme", "is that saso", "reunion" };
+			// ^ list of words
+			int r = rnd.Next(words.Count); // picks a random int based on the number of words
+			string word = (string)words[r]; // grab string at the selected int
+
+			return word; // return the string back to the program
+		}
+	}
+}
+
