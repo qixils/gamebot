b258f10afc27841ddf4d61905206163cd86d3fec
Noahkiq <noahkiqgaming@gmail.com>
2017-02-03 17:07:36 -0500

    Read dictionary from txt file

--- gamebot/Hangman.cs
+++ gamebot/Hangman.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using Discord;
+using System.IO;
 using System.Collections.Generic;
+using Discord;
 
 namespace gamebot
 {
 	public class Hangman
 	{
 		static Random rnd = new Random();
+		static string curdur = Directory.GetCurrentDirectory();
 
 		public Hangman()
 		{
@@ -15,10 +17,10 @@ namespace gamebot
 
 		public string WordChooser() // Chooses a random word from a custom dictionary
 		{
-			var words = new List<string> { "stabyourself", "meme", "is that saso", "reunion" };
-			// ^ list of words
-			int r = rnd.Next(words.Count); // picks a random int based on the number of words
-			string word = (string)words[r]; // grab string at the selected int
+			var dictionary = File.ReadAllLines (curdur + "/words.txt"); // grabs array of lines in 'words.txt' file
+
+			int r = rnd.Next(dictionary.Length); // picks a random int based on the number of words
+			string word = (string)dictionary[r]; // grab string at the selected int
 
 			return word; // return the string back to the program
 		}
